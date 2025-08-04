49cc2c1343df79cc8e21262e7bbe95f3f3a487f1
praveenpenumaka <praveen9920@gmail.com>
2025-08-05 00:52:48 +0530

    Update Docker workflow and install script for LibTorch
    - Updated torch version in Docker workflow to 2.7.1 and removed 2.3.1.
    - Simplified CUDA version handling in install_libtorch.sh by streamlining the get_cuda_id function.
    - Adjusted the installation script to echo the constructed LibTorch URL and commented out the extraction steps for clarity.

--- .github/workflows/scripts/install_libtorch.sh
+++ .github/workflows/scripts/install_libtorch.sh
@@ -2,26 +2,21 @@ CUDA_VERSION=$1
 TORCH_VERSION=$2
 
 get_cuda_id() {
-    local cuda_version=$1
-    local cuda_id=0
     # Remove the last digit from the cuda_version
-    cuda_version=${cuda_version%?}
+    CUDA_SMALLER_VERSION=${CUDA_VERSION%?}
     # Remove the . from the cuda_version
-    cuda_version=${cuda_version//./}
+    CUDA_SMALLER_VERSION=${CUDA_SMALLER_VERSION//./}
     # Add the cu prefix
-    CUDA_ID="cu$cuda_version"
-    echo $CUDA_ID
+    CUDA_ID="cu$CUDA_SMALLER_VERSION"
 }
 
 get_torch_url() {
-    cuda_version=$1
-    torch_version=$2
-    cuda_id=$(get_cuda_id $cuda_version)
-    torch_url="https://download.pytorch.org/libtorch/${cuda_id}/libtorch-cxx11-abi-shared-with-deps-${torch_version}%2B${cuda_id}.zip"
-    echo $torch_url
+    get_cuda_id
+    TORCH_URL="https://download.pytorch.org/libtorch/${CUDA_ID}/libtorch-cxx11-abi-shared-with-deps-${TORCH_VERSION}%2B${CUDA_ID}.zip"
 }
 
-TORCH_URL=$(get_torch_url $CUDA_VERSION $TORCH_VERSION)
+get_torch_url $CUDA_VERSION $TORCH_VERSION
+echo $TORCH_URL
 wget -q "$TORCH_URL" -O libtorch.zip
-unzip libtorch.zip -d /opt
-rm libtorch.zip
+# unzip libtorch.zip -d /opt
+# rm libtorch.zip
